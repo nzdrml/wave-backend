6de9a7afbb07a43c1570547bb6385cf7c0a889fa
Enzo <enzodoromal@gmail.com>
2016-11-05 13:29:16 +0800

    Create schedules for routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104141856) do
+ActiveRecord::Schema.define(version: 20161104145819) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 20161104141856) do
     t.integer "destination_id",                                         null: false
     t.index ["destination_id"], name: "index_routes_on_destination_id", using: :btree
     t.index ["origin_id"], name: "index_routes_on_origin_id", using: :btree
+  end
+
+  create_table "routes_schedules", id: false, force: :cascade do |t|
+    t.integer "route_id",    null: false
+    t.integer "schedule_id", null: false
+    t.index ["route_id", "schedule_id"], name: "index_routes_schedules_on_route_id_and_schedule_id", using: :btree
+    t.index ["schedule_id", "route_id"], name: "index_routes_schedules_on_schedule_id_and_route_id", using: :btree
+  end
+
+  create_table "schedules", force: :cascade do |t|
+    t.string "time", default: "06:30", null: false
   end
 
   create_table "users", force: :cascade do |t|
